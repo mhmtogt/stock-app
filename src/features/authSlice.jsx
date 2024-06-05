4e6857cb04510767446ee1cf9aae3edd40b14355
Mehmet OGUT <mogut44949@gmail.com>
2024-06-05 15:45:53 +0300

    authSlice

--- src/features/authSlice.jsx
+++ src/features/authSlice.jsx
@@ -1,11 +1,43 @@
 import { createSlice } from "@reduxjs/toolkit"
 
+// bizim burada login işlemini tutacak global statelere ihtiyacımız var 
+const initialState = {
+  user:"",
+  loading: false,
+   error: false,
+   token:"",
+
+
+
+}
+
+
+
+
+// biz slice'a yani global state veri aktarıcaz ama 
+//bu veri API den gelecek ya ayscn tunk yöndemiyle 
+//ekştra reucer yazacaz yada work arount yöntemi
+ 
 const authSlice = createSlice({
   name: "auth",
 
   initialState: {},
-  reducers: {},
-})
+  reducers: {
 
-export const {} = authSlice.actions
+    fetchStart:(state)=>{
+      state.loading=true
+    },
+    loginSuccess:(state, {payload})=>{
+      state.loading=false;
+      state.user= payload.user.username
+      state.token=payload.token
+    },
+    fetchFail:(state)=>{
+      state.loading=false;
+      state.error=true;
+    },
+  },
+})
+//
+export const {fetchStart,fetchFail,loginSuccess} = authSlice.actions
 export default authSlice.reducer
--- src/service/useAuthCalls.js
+++ src/service/useAuthCalls.js
@@ -1,21 +1,29 @@
 import axios from "axios";
 import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
 import { useNavigate } from "react-router-dom";
+import { fetchFail, fetchStart, loginSuccess } from "../features/authSlice";
+import { useDispatch } from "react-redux";
 
 const useAuthApiCalls = () => {
-  const navigate = useNavigate(); //"bu fonksyonun içerisine hook koymak gerekirse hooku bir js kodu içerisinde direk çağıramazsın bunun ya bir component olması gerekiyor yada custom hook olması gerekiyor  "
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  //"bu fonksyonun içerisine hook koymak gerekirse hooku bir js kodu içerisinde direk çağıramazsın bunun ya bir component olması gerekiyor yada custom hook olması gerekiyor  "
 
   //gireceğimiz verileri  bu fonksyon içerisinde uste belirtip  alta kullanacaz uzer insfo gibi
   const login = async (userInfo) => {
+    dispatch(fetchStart());
     try {
       const { data } = await axios.post(
         `${process.env.REACT_APP_BASE_URL}/auth/login`,
         userInfo
       );
+      dispatch(loginSuccess(data));
       toastSuccessNotify("Login islemi basarili");
       navigate("/stock");
-      console.log(data);
+      // console.log(data);
+      // return data;
     } catch (error) {
+      dispatch(fetchFail());
       toastErrorNotify("login islemi basarisiz oldu. ");
       console.log(error);
     }
